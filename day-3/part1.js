7b995e364327c27cde850893aebb76b6f5e6ea65
adrian154 <94568a@gmail.com>
2021-12-02 23:21:01 -0800

    get rid of js it stinks anyway

--- day-3/part1.js
+++
@@ -1,6 +0,0 @@
-console.log(
-    (n => console.log(n))(
-    (lines => [...lines[0].keys()].map(col => lines.reduce((a, line) => a + (line[col] == '1'), 0)).join(""))
-    (require("fs").readFileSync("data.txt", "utf-8").split("\n").map(line => line.split("")))
-    )
-);
